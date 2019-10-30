a8ac60fefc3d0654ba327241c262ebac4c8ca4b7
by <just_by@naver.com>
2019-10-30 15:31:36 +0900

    Add files via upload
    2019 10 30 MFC 과제

---
+++ Dlg7/CSonDlg.cpp
@@ -0,0 +1,45 @@
+﻿// CSonDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "Dlg7.h"
+#include "CSonDlg.h"
+#include "afxdialogex.h"
+
+
+// CSonDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CSonDlg, CDialogEx)
+
+CSonDlg::CSonDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+CSonDlg::~CSonDlg()
+{
+}
+
+void CSonDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CSonDlg, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &CSonDlg::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// CSonDlg 메시지 처리기
+
+#include "Dlg7Dlg.h"
+void CSonDlg::OnBnClickedButton1()
+{
+	CDlg7Dlg* p = (CDlg7Dlg*)GetParent();
+	CString m;
+	GetDlgItemText(IDC_EDIT1, m);
+	p->m_List.AddString(m);
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
---
+++ Dlg7/CSonDlg.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+
+// CSonDlg 대화 상자
+
+class CSonDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CSonDlg)
+
+public:
+	CSonDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CSonDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+};
